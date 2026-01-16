5cf80b208bd0feaa927667c81ba432c9a3399a3d
Attaullahsw <53965225+Attaullahsw@users.noreply.github.com>
2026-01-16 12:28:10 +0000

    Deploying to gh-pages from @ Attaullahsw/attaullahsw.github.io@3846842ab3897a89ec9be9dc478286a6adb3c402 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Attaullahsw/attaullah.github.io",
+    "data-repo": "Attaullahsw/attaullahsw.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
